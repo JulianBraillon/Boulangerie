a6ad9319ae12b91436ccc544077d631cc72f718f
Julian <jbraillon6@gmail.com>
2025-11-03 17:58:39 +0100

    first commit

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/Accueil.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <title>Accueil - Le pain d'Antan</title>
+</head>
+
+<body>
+    <div class="body-1">
+        <div class="imageArrierePlan">
+            <h1>Le Pain d'Antan</h1>
+            <p>" Le vrai goût du temps passé "</p>
+            <a href="#">Découvrir nos créations</a>
+        </div>
+    </div>
+    <div class="body-2">
+        <div class="body-2-grand-mere">
+            <h2>Mise en avant des <br>produits du jour</h2>
+            <div class="body-2-mere">
+                <div class="body-2-fille"></div>
+                <div class="body-2-fille"></div>
+                <div class="body-2-fille"></div>
+            </div>
+            <a href="#">Ajouter au panier</a>
+        </div>
+    </div>
+</body>
+
+</html>
